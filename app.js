7fc8bca2ed8923f1708b3a5d8c48897e2054dfe1
notalent2code <rihlanrihlan24@gmail.com>
2023-04-20 23:02:31 +0700

    add cors and prisma

--- app.js
+++ app.js
@@ -2,15 +2,23 @@ const createError = require('http-errors');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
+const dotenv = require('dotenv');
 
 const indexRouter = require('./routes/index');
 
+dotenv.config();
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors({
+  origin: process.env.WEB_DOMAIN,
+  credentials: true
+}))
+
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
