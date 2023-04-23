2f198df95c2523747ec823c1949899319683f7de
notalent2code <rihlanrihlan24@gmail.com>
2023-04-23 22:51:58 +0700

    add 404 invalid route handler

--- app.js
+++ app.js
@@ -2,41 +2,36 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 const cors = require("cors");
-const createError = require("http-errors");
+const helmet = require("helmet");
 const dotenv = require("dotenv");
 
 const indexRouter = require("./src/routes/index");
+const authRouter = require("./src/routes/auth");
 
 dotenv.config();
 const app = express();
 
+// Middlewares
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(
   cors({
     origin: process.env.WEB_DOMAIN,
     credentials: true,
   })
 );
-app.use(morgan("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
-app.use("/", indexRouter);
+// Routes
+app.use("/api/", indexRouter);
+app.use("/api/auth", authRouter);
 
-// catch 404 and forward to error handler
-app.use(function(next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// The 404 Route
+app.get("*", function (_req, res) {
+  res.status(404).json({ message: "Page Not Found" });
 });
 
 module.exports = app;
