1e28570f087244639686543a3d51313854a735d2
notalent2code <rihlanrihlan24@gmail.com>
2023-04-23 20:08:02 +0700

    chore: add helmet, bcrypt, and jsonwebtoken deps

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
-const createError = require('http-errors');
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors');
-const dotenv = require('dotenv');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const cors = require("cors");
+const createError = require("http-errors");
+const dotenv = require("dotenv");
 
-const indexRouter = require('./routes/index');
+const indexRouter = require("./src/routes/index");
 
 dotenv.config();
 const app = express();
 
-app.use(logger('dev'));
+app.use(
+  cors({
+    origin: process.env.WEB_DOMAIN,
+    credentials: true,
+  })
+);
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors({
-  origin: process.env.WEB_DOMAIN,
-  credentials: true
-}))
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function(next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
+const { PrismaClient } = require("@prisma/client");
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({ message: 'Hello World' })
+router.get("/", function (req, res, next) {
+  res.status(200).json("Hello World!");
+});
+
+router.post("/register", async function (req, res, next) {
+  const { email, passwordHash, firstName, lastName, phoneNumber, userType } = req.body;
+  console.log()
+  const prisma = new PrismaClient();
+  const user = await prisma.users.create({
+    data: req.body,
+  });
+
+  res.status(201).json({
+    "msg": "User registered successfully",
+    "user": user
+  });
 });
 
 module.exports = router;
