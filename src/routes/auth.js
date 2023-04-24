b9af6ce1b378c1cb985f3a2c3b2e5f411bac6b15
notalent2code <rihlanrihlan24@gmail.com>
2023-04-24 15:16:20 +0700

    feat: add logout and refresh token route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,10 +2,17 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth");
 
+
 // POST /auth/register
 router.post("/register", authController.register);
 
 // POST /auth/login
 router.post("/login", authController.login);
+
+// DELETE /auth/logout
+router.delete("/logout", authController.logout);
+
+// GET /auth/refresh-token
+router.get("/refresh-token", authController.refreshToken);
 
 module.exports = router;
