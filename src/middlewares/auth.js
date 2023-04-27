5c15b7141524f2bb3616f9ba6efb179909e47ea1
notalent2code <rihlanrihlan24@gmail.com>
2023-04-27 20:58:31 +0700

    fix: edit auth middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -8,7 +8,7 @@ const verifyToken = async (req, res, next) => {
     const authHeader = req.header("Authorization");
     const token = authHeader && authHeader.split(" ")[1];
 
-    if (token == null) return res.status(401).send("Access denied");
+    if (!token) return res.status(401).send("Access denied");
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
       if (err) return res.status(403).send("Invalid token");
