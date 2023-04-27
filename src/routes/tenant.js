ead84eecfbc62a4394cd1793813a09a6f462240b
notalent2code <rihlanrihlan24@gmail.com>
2023-04-27 20:46:19 +0700

    feat: add tenant view and edit profile routes

---
+++ src/routes/tenant.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const verifyToken = require("../middlewares/auth");
+const tenantController = require("../controllers/tenant");
+const upload = require("../utils/uploadImage");
+
+const router = express.Router();
+
+// GET /tenants/:tenantId/profile
+router.get("/:tenantId/profile", verifyToken, tenantController.getTenantProfile);
+
+// PUT /tenants/:tenantId/profile
+router.put("/:tenantId/profile", verifyToken, upload.single('avatarUrl'), tenantController.updateTenantProfile);
+
+module.exports = router;
