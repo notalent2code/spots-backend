285af7a22bb1b8d63f8692c2060a7ff65e49cf06
notalent2code <rihlanrihlan24@gmail.com>
2023-05-07 22:01:36 +0700

    owner view and edit route

--- src/routes/owner.js
+++ src/routes/owner.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const verifyToken = require("../middlewares/auth");
 const ownerController = require("../controllers/owner");
-const upload = require("../utils/uploadImage");
+const { ktpUpload } = require("../utils/uploadImage");
 
 const router = express.Router();
 
 // GET /owners/:ownerId/info
-router.get("/:ownerId/info", verifyToken, upload.single('ktpURL'), ownerController.getOwnerInfo);
+router.get("/:ownerId/info", verifyToken, ownerController.getOwnerInfo);
+
+// PUT /owners/:ownerId/info
+router.put("/:ownerId/info", verifyToken, ktpUpload.single('ktpURL'), ownerController.updateOwnerInfo);
+
+module.exports = router;
