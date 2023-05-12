f921742b4f5fdb3884e65b75602a63948c60b76a
notalent2code <rihlanrihlan24@gmail.com>
2023-05-12 11:49:10 +0700

    add tenant middleware

---
+++ src/middlewares/tenant.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const verifyTenant = async (req, res, next) => {
+  try {
+    const authHeader = req.header("Authorization");
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (!token) return res.status(401).send("Access denied");
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) return res.status(403).send("Invalid token");
+      if (decoded.userType !== "TENANT")
+        return res.status(403).send("Tenant access denied");
+      req.user = decoded;
+      next();
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = verifyTenant;
