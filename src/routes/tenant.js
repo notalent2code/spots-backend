8070c03893688a0346b5c67dec66a911b716484d
notalent2code <rihlanrihlan24@gmail.com>
2023-05-12 11:53:17 +0700

    refactor tenant routes

--- src/routes/tenant.js
+++ src/routes/tenant.js
@@ -1,14 +1,13 @@
 const express = require("express");
-const verifyToken = require("../middlewares/auth");
 const tenantController = require("../controllers/tenant");
 const { avatarUpload } = require("../utils/uploadImage");
 
 const router = express.Router();
 
 // GET /tenants/:tenantId/profile
-router.get("/:tenantId/profile", verifyToken, tenantController.getTenantProfile);
+router.get("/:tenantId/profile", tenantController.getTenantProfile);
 
 // PUT /tenants/:tenantId/profile
-router.put("/:tenantId/profile", verifyToken, avatarUpload.single('avatarURL'), tenantController.updateTenantProfile);
+router.put("/:tenantId/profile", avatarUpload.single('avatarURL'), tenantController.updateTenantProfile);
 
 module.exports = router;
