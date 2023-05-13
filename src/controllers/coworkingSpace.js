f9343b10a017cb646da03815965756417895f80f
notalent2code <rihlanrihlan24@gmail.com>
2023-05-13 20:54:53 +0700

    feat: get all, get by id and refactor code logic

--- src/controllers/coworkingSpace.js
+++ src/controllers/coworkingSpace.js
@@ -1,33 +1,132 @@
 const { PrismaClient } = require("@prisma/client");
 const fs = require("fs");
 const path = require("path");
+const jwt = require("jsonwebtoken");
 
 const prisma = new PrismaClient();
 
-const getCoworkingSpaces = async (req, res) => {
-  try {
-    const coworkingSpaces = await prisma.coworkingSpace.findMany({
-      include: {
-        owner: {
+const prismaStatement = {
+  include: {
+    location: true,
+    // availabilities: true,
+    coworking_space_images: {
+      select: {
+        image_url: true,
+      },
+    },
+    coworking_space_facilities: {
+      select: {
+        facility: {
           select: {
-            user: {
-              select: {
-                first_name: true,
-                last_name: true,
-                email: true,
-                phone_number: true,
-              },
-            },
+            facility_id: true,
+            name: true,
+            description: true,
           },
         },
-        location_id: true,
-        availabilities: true,
-        coworking_space_images: true,
-        coworking_space_facilities: true,
       },
-    });
+    },
+  },
+};
+
+const getCoworkingSpaces = async (req, res) => {
+  try {
+    const authHeader = req.header("Authorization");
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (token) {
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        req.user = decoded;
+      });
+    }
+
+    let coworkingSpaces;
+
+    if (req.user && req.user.userType === "OWNER") {
+      const owner = await prisma.owner.findUnique({
+        where: {
+          user_id: parseInt(req.user.userId),
+        },
+      });
+
+      coworkingSpaces = await prisma.coworkingSpace.findMany({
+        where: {
+          owner_id: owner.owner_id,
+        },
+        ...prismaStatement,
+      });
+    } else if (req.user && req.user.userType === "ADMIN") {
+      coworkingSpaces = await prisma.coworkingSpace.findMany({
+        ...prismaStatement,
+      });
+    } else {
+      coworkingSpaces = await prisma.coworkingSpace.findMany({
+        where: {
+          status: "APPROVED",
+        },
+        ...prismaStatement,
+      });
+    }
+    if (!coworkingSpaces) {
+      return res.status(404).json({ message: "Coworking spaces not found" });
+    }
+
     return res.status(200).json({ coworkingSpaces });
   } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const getCoworkingSpaceById = async (req, res) => {
+  try {
+    const authHeader = req.header("Authorization");
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (token) {
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        req.user = decoded;
+      });
+    }
+
+    let coworkingSpace;
+
+    if (req.user && req.user.userType === "OWNER") {
+      const owner = await prisma.owner.findUnique({
+        where: {
+          user_id: parseInt(req.user.userId),
+        },
+      });
+
+      coworkingSpace = await prisma.coworkingSpace.findFirst({
+        where: {
+          owner_id: owner.owner_id,
+          space_id: parseInt(req.params.spaceId),
+        },
+        ...prismaStatement,
+      });
+    } else if (req.user && req.user.userType === "ADMIN") {
+      coworkingSpace = await prisma.coworkingSpace.findUnique({
+        where: {
+          space_id: parseInt(req.params.spaceId),
+        },
+        ...prismaStatement,
+      });
+    } else {
+      coworkingSpace = await prisma.coworkingSpace.findFirst({
+        where: {
+          space_id: parseInt(req.params.spaceId),
+          status: "APPROVED",
+        },
+        ...prismaStatement,
+      });
+    }
+
+    if (!coworkingSpace) {
+      return res.status(404).json({ message: "Coworking space not found" });
+    }
+
+    return res.status(200).json({ coworkingSpace });
+  } catch (error) {
+    console.log(error);
     return res.status(500).json({ message: error.message });
   }
 };
@@ -55,10 +154,22 @@ const addCoworkingSpace = async (req, res) => {
       facilities,
     } = req.body;
 
-    if (req.files.length === 0) {
+    if (req.files == null || req.files.length === 0) {
       return res
         .status(400)
         .json({ message: "Please upload at least one image" });
+    }
+
+    const existingCoworkingSpace = await prisma.coworkingSpace.findUnique({
+      where: {
+        name,
+      },
+    });
+
+    if (existingCoworkingSpace) {
+      return res
+        .status(400)
+        .json({ message: "Coworking space with this name already exists" });
     }
 
     const resCoworkingSpace = await prisma.coworkingSpace.create({
@@ -167,10 +278,25 @@ const updateCoworkingSpace = async (req, res) => {
       return res.status(403).json({ message: "Please verify your account" });
     }
 
-    const coworkingSpace = await prisma.coworkingSpace.findUnique({
+    const coworkingSpace = await prisma.coworkingSpace.findFirst({
       where: {
         owner_id: verifiedOwner.owner_id,
         space_id: parseInt(req.params.spaceId),
+      },
+      include: {
+        location: true,
+        coworking_space_images: true,
+        coworking_space_facilities: {
+          select: {
+            facility: {
+              select: {
+                facility_id: true,
+                name: true,
+                description: true,
+              },
+            },
+          },
+        },
       },
     });
 
@@ -224,7 +350,7 @@ const updateCoworkingSpace = async (req, res) => {
 
     let resImages;
 
-    if (req.files.length > 0) {
+    if (req.files && req.files.length > 0) {
       const reqImages = req.files.map((file) => {
         return {
           space_id: coworkingSpace.space_id,
@@ -232,15 +358,21 @@ const updateCoworkingSpace = async (req, res) => {
         };
       });
 
-      const deleteOldImages = await prisma.coworkingSpaceImage.deleteMany({
+      const deleteOldImages = await prisma.coworkingSpaceImage.findMany({
         where: {
           space_id: coworkingSpace.space_id,
         },
       });
 
       if (!deleteOldImages) {
-        return res.status(400).json({ message: "Failed to delete old images" });
+        return res.status(400).json({ message: "No images found" });
       }
+
+      await prisma.coworkingSpaceImage.deleteMany({
+        where: {
+          space_id: coworkingSpace.space_id,
+        },
+      });
 
       for (oldImage of deleteOldImages) {
         const oldImagePath = oldImage.image_url.split(
@@ -258,13 +390,13 @@ const updateCoworkingSpace = async (req, res) => {
       if (!images) {
         return res.status(400).json({ message: "Failed to edit images" });
       }
-
-      resImages = await prisma.coworkingSpaceImage.findMany({
-        where: {
-          space_id: coworkingSpace.space_id,
-        },
-      });
     }
+
+    resImages = await prisma.coworkingSpaceImage.findMany({
+      where: {
+        space_id: coworkingSpace.space_id,
+      },
+    });
 
     let resFacilities;
 
@@ -305,17 +437,17 @@ const updateCoworkingSpace = async (req, res) => {
             facility_id: facility.facility_id,
           },
         });
-
-        resFacilities = await prisma.coworkingSpaceFacility.findMany({
-          where: {
-            space_id: coworkingSpace.space_id,
-          },
-          include: {
-            facility: true,
-          },
-        });
       }
     }
+
+    resFacilities = await prisma.coworkingSpaceFacility.findMany({
+      where: {
+        space_id: coworkingSpace.space_id,
+      },
+      include: {
+        facility: true,
+      },
+    });
 
     return res.status(200).json({
       message: "Edit coworking space success",
@@ -331,6 +463,7 @@ const updateCoworkingSpace = async (req, res) => {
 
 module.exports = {
   getCoworkingSpaces,
+  getCoworkingSpaceById,
   addCoworkingSpace,
   updateCoworkingSpace,
 };
